9647f219625a2e14e7302ecef0e4d5eef65f79fd
haowang <864745998@qq.com>
2026-01-16 17:39:49 +0800

    增加注释

--- build_debug.sh
+++ build_debug.sh
@@ -1,6 +1,5 @@
 rm -rf build
 mkdir build
+cd build
 cmake -DCMAKE_PREFIX_PATH="D:/QT/6.10.1/msvc2022_64"  -DOpenCASCADE_DIR="D:/occt-install/occt-7.6.0-vc143/cmake" -DCMAKE_BUILD_TYPE=Release ..
 cmake --build . --config Release
-
-# conan install . -of build -s build_type=Debug 
--- src/app/MainWindow.cpp
+++ src/app/MainWindow.cpp
@@ -9,7 +9,7 @@
 MainWindow::MainWindow(QWidget* parent)
   : QMainWindow(parent)
 {
-  setWindowTitle("MiniCAD (Qt + OCCT)");
+  setWindowTitle("MiniCAD");
 
   m_viewport = new OccViewportWidget(this);
   setCentralWidget(m_viewport);
@@ -17,6 +17,8 @@ MainWindow::MainWindow(QWidget* parent)
   auto* viewMenu = menuBar()->addMenu(tr("View"));
   auto* fitAllAction = new QAction(tr("Fit All"), this);
   fitAllAction->setShortcut(QKeySequence(Qt::Key_F));
+  //在 Qt 里，信号（signal）= 事件通知，槽（slot）= 事件处理函数。
+  //connect(A的信号, B的槽) 的意思就是：A 发生某个事件时，自动调用 B 的某个函数。
   connect(fitAllAction, &QAction::triggered, m_viewport, &OccViewportWidget::fitAll);
   viewMenu->addAction(fitAllAction);
 
